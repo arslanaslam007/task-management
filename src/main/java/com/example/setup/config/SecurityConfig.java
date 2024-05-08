deaff61dfc17b6a15366f36b3f83ba068b3b3dcb
Arslan Aslam <arslanaslam428@gmail.com>
2024-05-08 17:18:03 +0500

    role based authorization

--- src/main/java/com/example/setup/config/SecurityConfig.java
+++ src/main/java/com/example/setup/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,9 +21,29 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(reg -> {
             reg.requestMatchers("/").permitAll();
-            reg.requestMatchers("/api/**").authenticated();
+            reg.requestMatchers("/api/user/**").hasRole("USER");
+            reg.requestMatchers("/api/admin/**").hasRole("ADMIN");
+            reg.anyRequest().authenticated();
         });
         http.httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user = User.builder()
+                .username("user")
+                .password("$2a$12$.mYdT0quk43vPBNjglyfSeJoOLd7KCWDQxUBAODCVftNuiCFra7Ee")
+                .roles("USER")
+                .build();
+        var admin = User.builder()
+                .username("admin")
+                .password("$2a$12$OaedKjZPKcHIxNf0sist5.N/zN0A1rRE0Ot48ERc7DX4r2lCuT/2G")
+                .roles("ADMIN","USER")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
